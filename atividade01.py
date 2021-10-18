02caf386a61dd814601cccc8ac0d750cd5579104
Pedro Freitas <89426809+Pfreitas20@users.noreply.github.com>
2021-10-18 17:33:45 -0300

    primeiro commit

---
+++ atividade01.py
@@ -0,0 +1,21 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+n1= float(input("digite a primeira nota:"))
+n2= float(input("digite a segunda nota:"))
+n3= float(input("digite a terceira nota:"))
+n4= float(input("digite a quarta nota:"))
+
+#media todos o valores dividos pela quantidade 
+
+print("A media sera:",(n1+n2+n3+n4/4))
+
+
+
+#git init 
+#git add README.md 
+#git push -u origin main
+#git init 
+#git add README.md 
+#git commit -m "first commit" 
+#git branch -M main 
+#git remote add origin https://github.com/Pfreitas20/ praticando-pyhton.github.io.git
+#git push -u origin main
